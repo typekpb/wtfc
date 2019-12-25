553efe2df4b8fd42f4cb796fa7d55c89c1bb25fd
Peter Butkovic <butkovic@gmail.com>
2019-12-25 21:19:35 +0100

    test: fix alpine tests

--- shpec/wtfc_shpec.sh
+++ shpec/wtfc_shpec.sh
@@ -1,11 +1,12 @@
-# timeout error status differs based on distro (busybox/alpine=143, others=124)
+# timeout error status differs based on distro (alpine=143, others=124)
 timeout_err_status() {
-    TIMEOUT_FLAG_TEST="$(timeout 1 sleep 0 2>&1)"
-    case "${TIMEOUT_FLAG_TEST}" in
-        timeout:\ can\'t\ execute\ \'1\':*) return 143 ;;
-        *) return 124 ;;
-    esac
-
+    cat /etc/os-release | grep ID= | grep -q alpine
+    ec=$?
+    if [ $ec -eq 0 ] ; then
+        return 143
+    else
+        return 124
+    fi
 }
 
 describe "-P, --progress argument"
@@ -44,12 +45,12 @@ end
 
 describe "-V, --version argument"
     it "-V argument exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh -V >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -V >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "--version argument exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh --version >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh --version >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
@@ -66,100 +67,100 @@ end
 
 describe "-H, --help argument"
     it "-H argument exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh -H >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -H >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "--help argument exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh --help >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh --help >/dev/null 2>&1
         assert equal "$?" "0"
     end
 end
 
 describe "-T, --timeout argument"
     it "'-T 3' with command taking 2 seconds, exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh -T 3 sleep 2 >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -T 3 sleep 2 >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "'-timeout=3' with command taking 2 seconds, exit status is 0"
-        $SHPEC_ROOT/../wtfc.sh --timeout=3 sleep 2 >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh --timeout=3 sleep 2 >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "'-T 1' with command taking 2 seconds, exit status is not 0"
-        $SHPEC_ROOT/../wtfc.sh -T 1 sleep 2 >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -T 1 sleep 2 >/dev/null 2>&1
         assert unequal "$?" "0"
     end
 
     it "'--timeout=1' with command taking 2 seconds, exit status is not 0"
-        $SHPEC_ROOT/../wtfc.sh --timeout=1 sleep 2 >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh --timeout=1 sleep 2 >/dev/null 2>&1
         assert unequal "$?" "0"
     end
 end
 
 describe "COMMAND read from stdin"
     it "exit status 1 is evaluated as 1"
-        echo 'echo aaa | grep -q zzz' | $SHPEC_ROOT/../wtfc.sh >/dev/null 2>&1 
+        echo 'echo aaa | grep -q zzz' | $SHPEC_ROOT/../wtfc.sh >/dev/null 2>&1
         assert equal "$?" "1"
     end
     it "exit status 0 is evaluated as 0"
-        echo 'echo aaa | grep -q aaa' | $SHPEC_ROOT/../wtfc.sh >/dev/null 2>&1 
+        echo 'echo aaa | grep -q aaa' | $SHPEC_ROOT/../wtfc.sh >/dev/null 2>&1
         assert equal "$?" "0"
     end
 end
 
 describe "COMMAND with pipe"
     it "exit status 1 is evaluated as 1"
-        $SHPEC_ROOT/../wtfc.sh 'echo aaa | grep -q zzz' >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh 'echo aaa | grep -q zzz' >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
     it "exit status 0 is evaluated as 0"
-        $SHPEC_ROOT/../wtfc.sh 'echo aaa | grep -q aaa' >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh 'echo aaa | grep -q aaa' >/dev/null 2>&1
         assert equal "$?" "0"
     end
 end
 
 describe "COMMAND exit status"
     it "returns 0 if expected as well as actual were 0"
-        $SHPEC_ROOT/../wtfc.sh -S 0 ls >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -S 0 ls >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
     it "returns 0 if expected (not provided => implicit = 0) as well as actual was 0"
-        $SHPEC_ROOT/../wtfc.sh ls >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh ls >/dev/null 2>&1
         assert equal "$?" "0"
     end
 
-    it "returns 123/143 if expected is 0, but actual was not 0"
+    it "returns 124/143 if expected is 0, but actual was not 0"
         timeout_err_status
         timeout_status="$?"
-        $SHPEC_ROOT/../wtfc.sh -S 0 ls /nonexistant/dir >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -S 0 ls /nonexistant/dir >/dev/null 2>&1
         assert equal "$?" "${timeout_status}"
     end
 
-    it "returns 123/143 if expected is not 0, but actual was 0"
-        $SHPEC_ROOT/../wtfc.sh -S 2 ls >/dev/null 2>&1 
+    it "returns 124/143 if expected is not 0, but actual was 0"
+        $SHPEC_ROOT/../wtfc.sh -S 2 ls >/dev/null 2>&1
         assert equal "$?" "${timeout_status}"
     end
-    
+
     it "returns 0 if expected as well as actual were non-zero but equal"
         timeout_err_status
         timeout_status="$?"
-        $SHPEC_ROOT/../wtfc.sh -S ${timeout_status} ls /nonexistant/dir >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -S ${timeout_status} ls /nonexistant/dir >/dev/null 2>&1
         assert equal "$?" "0"
     end
 end
 
 describe "Unknown argument"
     it "-Z argument exit status is 1"
-        $SHPEC_ROOT/../wtfc.sh -Z >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh -Z >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
     it "--zzz argument exit status is 1"
-        $SHPEC_ROOT/../wtfc.sh --zzz >/dev/null 2>&1 
+        $SHPEC_ROOT/../wtfc.sh --zzz >/dev/null 2>&1
         assert equal "$?" "1"
     end
 
@@ -167,7 +168,7 @@ describe "Unknown argument"
         message="$($SHPEC_ROOT/../wtfc.sh -Z 2>&1 > /dev/null)"
         assert grep "$message" "Unknown argument: -Z"
     end
-    
+
     it "-Z argument prints nothing to stdout"
         message="$($SHPEC_ROOT/../wtfc.sh -Z 2>/dev/null)"
         assert grep "$message" ""
